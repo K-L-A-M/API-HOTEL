8608c9ecb5fe6255b8fc73e4a6e2d617c2b1ce90
MayconFeh <mfbm87@gmail.com>
2023-11-10 11:34:57 -0300

    feat: models do rooms feita e migração do rooms

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+
+class RoomFeature(models.Model):
+    name = models.CharField(unique=True)
+
+
+class BedType(models.TextChoices):
+    SINGLE = 'Single'
+    DOUBLE = 'Double'
+    KING_SIZE = 'King Size'
+    QUEEN_SIZE = 'Queen Size'
+
+
+class Room(models.Model):
+    class Meta:
+        ordering = ['id',]
+
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    floor = models.PositiveIntegerField(null=False)
+    room_number = models.CharField(max_length=10, unique=True, null=False)
+    capacity = models.PositiveIntegerField(null=False)
+    is_occupied = models.BooleanField(default=False)
+    price_per_night = models.DecimalField(max_digits=8, decimal_places=2, null=False)
+    # promotions = models.ManyToManyField(Promotion, blank=True)
+    beds = models.ManyToManyField('Bed', blank=True, related_name='bed_rooms')
+    features = models.ManyToManyField(RoomFeature, blank=True)
+
+
+class Bed(models.Model):
+    bed_type = models.CharField(max_length=20, choices=BedType.choices, null=False)
+    quantity = models.PositiveIntegerField(null=False)
+    room = models.ForeignKey('Room', related_name='room_beds', on_delete=models.CASCADE)
