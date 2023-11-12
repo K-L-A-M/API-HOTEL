7a8df4d7902197ff1c0facbbdb08dd1121d4c8aa
MayconFeh <mfbm87@gmail.com>
2023-11-12 09:45:09 -0300

    feat:models  da transactions feita e migração

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from uuid import uuid4
+from users.models import User
 
-# Create your models here.
+
+class TransactionMethod(models.TextChoices):
+    CREDIT_CARD = "Credit Card"
+    DEBIT_CARD = "Debit Card"
+    CASH = "Cash"
+    PIX = "Pix"
+
+
+class Transaction(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_cpf = models.CharField(max_length=14, null=True, blank=True)
+    user_name = models.CharField(max_length=150, null=True, blank=True)
+    method = models.CharField(choices=TransactionMethod.choices, max_length=20)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    amount_paid = models.DecimalField(max_digits=8, decimal_places=2, validators=[MinValueValidator(0)])
+    discount_percentage = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
+    discount_amount = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+
+    def save(self, *args, **kwargs):
+        self.user_cpf = self.user.cpf
+        self.user_name = self.user.name
+        super().save(*args, **kwargs)
